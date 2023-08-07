26344a1843bb14515ee6f37cb806604d0772d9db
RageNeko26 <rneko2006@gmail.com>
2023-08-07 14:43:21 +0700

    feat: Add Keypair and asymmetric

--- asymmetric/main.go
+++ asymmetric/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"crypto/ed25519"
+	"encoding/hex"
 	"fmt"
+	"time"
+
+	"github.com/o1egl/paseto"
 )
 
+var privByte, _ = hex.DecodeString("b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZWQyNTUxOQAAACCX0Dw8NavIwFDX1811Tn4+35UNJT3qLdAkcbeZiyErYgAAAKh+4OANfuDgDQAAAAtzc2gtZWQyNTUxOQAAACCX0Dw8NavIwFDX1811Tn4+35UNJT3qLdAkcbeZiyErYgAAAED9XNY2RHnAGqv6gzGJ71SyjB5JWbiPxwJF5Rluc5nPupfQPDw1q8jAUNfXzXVOfj7flQ0lPeot0CRxt5mLIStiAAAAInJpZGhvZ2FweEByaWRob2dhcHgtQXNwaXJlLVIzLTEzMVQBAgM=")
+var privKey = ed25519.PrivateKey(privByte)
+var pubByte, _ = hex.DecodeString("AAAAC3NzaC1lZDI1NTE5AAAAIJfQPDw1q8jAUNfXzXVOfj7flQ0lPeot0CRxt5mLISti")
+var pubKey = ed25519.PublicKey(pubByte)
+
+func Encode() string {
+	payload := paseto.JSONToken{
+		Expiration: time.Now().Add(24 * time.Hour),
+		Subject:    "Research",
+	}
+
+	payload.Set("data", "Life is Roblox!")
+
+	footer := "Copyright"
+
+	token, err := paseto.NewV2().Sign(privKey, payload, footer)
+
+	if err != nil {
+		panic(err)
+	}
+	return token
+}
+
 func main() {
-	publicKey, privateKey, _ := ed25519.GenerateKey(nil)
+	token := Encode()
 
-	pub := []byte(publicKey)
-	p := []byte(privateKey)
-
-	fmt.Printf("Public Key: %v \n", string(pub))
-	fmt.Printf("Private Key: %v \n", string(p))
-
+	fmt.Println(token)
 }
