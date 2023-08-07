791884a91bcf674f743384c5097b08676cb0588d
RageNeko26 <rneko2006@gmail.com>
2023-08-07 13:50:20 +0700

    fix:Delete unused code

--- uuid_generator/main.go
+++ uuid_generator/main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"crypto/ed25519"
-	"fmt"
-)
-
 func main() {
-	publicKey, privateKey, _ := ed25519.GenerateKey(nil)
-
-	fmt.Printf("Public Key: %v", publicKey)
-	fmt.Printf("Private Key: %v", privateKey)
 
 }
