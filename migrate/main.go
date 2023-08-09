0958107430c642f6507539e092c9479ac04f2156
RageNeko26 <rneko2006@gmail.com>
2023-08-09 14:34:05 +0700

    fix: Learn how to embed

--- migrate/main.go
+++ migrate/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"embed"
+	"fmt"
+	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
+)
+
+var fs embed.FS
+
+func main() {
+	d, err := iofs.New(fs, "data/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, errD := migrate.NewWithSourceInstance("iofs", d, "postgres://root:root@localhost/cvz_auth?sslmode=disable")
+
+	if errD != nil {
+		log.Fatal(err)
+	}
+
+	errS := m.Up()
+
+	if errS != nil {
+		log.Fatal(errS)
+	}
+
+	fmt.Println("Migrating...")
 }
