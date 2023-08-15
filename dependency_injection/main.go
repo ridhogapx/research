f4291a7ab6471994c83ddec666f2f1e88d380b6d
RageNeko26 <rneko2006@gmail.com>
2023-08-15 13:14:55 +0700

    refactor: clean up a bit

--- dependency_injection/main.go
+++ dependency_injection/main.go
@@ -4,14 +4,14 @@ type Message string
 
 type Event struct{}
 
+type Greeter struct {
+	Message Message
+}
+
 func NewMessage() Message {
 	return Message("Hi, buddy!")
 }
 
 func NewGreeter(m Message) Greeter {
 	return Greeter{Message: m}
-}
-
-type Greeter struct {
-	Message Message
 }
