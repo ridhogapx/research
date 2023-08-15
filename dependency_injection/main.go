ae562a6b5af3bf714f77c3ea361951e89d9fcf5e
RageNeko26 <rneko2006@gmail.com>
2023-08-15 13:09:11 +0700

    feat: Add initializer

--- dependency_injection/main.go
+++ dependency_injection/main.go
@@ -2,10 +2,16 @@ package main
 
 type Message string
 
-type Greeter struct{}
-
 type Event struct{}
 
 func NewMessage() Message {
 	return Message("Hi, buddy!")
+}
+
+func NewGreeter(m Message) Greeter {
+	return Greeter{Message: m}
+}
+
+type Greeter struct {
+	Message Message
 }
