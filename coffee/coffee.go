71574dd0a1339941b98993c460d45a9820bd81d7
RageNeko26 <rneko2006@gmail.com>
2023-08-16 12:38:36 +0700

    feat: Injecting coffee

---
+++ coffee/coffee.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/wire"
+)
+
+type Coffee struct {
+	Name     string
+	Quantity int8
+	Owner    string
+}
+
+type Order struct {
+	Coffee Coffee
+	Waiter string
+}
+
+func NewCoffee() Coffee {
+	return Coffee{
+		Name:     "Caffuchino",
+		Quantity: 1,
+		Owner:    "John",
+	}
+}
+
+func NewOrder(coffee Coffee) Order {
+	return Order{
+		Coffee: coffee,
+		Waiter: "Doe",
+	}
+}
+
+func (order Order) Ordering() {
+	fmt.Printf("%v ordering %v to %v", order.Coffee.Owner, order.Coffee.Name, order.Waiter)
+}
+
+func InitializeCoffee() Order {
+	wire.Build(NewOrder, NewCoffee)
+	return Order{}
+}
