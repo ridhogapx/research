5f349f33fae24dd2704b913f7d0f3f39def8f45b
RageNeko26 <rneko2006@gmail.com>
2023-08-18 18:44:18 +0700

    feat: Implement method service

--- exp_grpc/greet.go
+++ exp_grpc/greet.go
@@ -1,9 +1,20 @@
 package main
 
-import "research/exp_grpc/proto"
+import (
+	"fmt"
+	"research/exp_grpc/proto"
+)
 
 type Server struct {
 	proto.UnimplementedGreeterServer
 }
 
-func (*Server) SayHello(req *proto.HelloRequest)
+func (*Server) SayHello(req *proto.HelloRequest, stream proto.Greeter_SayHelloServer) error {
+	someone := req.Name
+
+	stream.Send(&proto.HelloResponse{
+		Message: fmt.Sprintf("Keep it up! %s", someone),
+	})
+
+	return nil
+}
