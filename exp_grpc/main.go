8e1970ae5137dd6d753ab127d43690c11a7b7dcb
RageNeko26 <rneko2006@gmail.com>
2023-08-18 18:48:41 +0700

    feat: Setup RPC Server

--- exp_grpc/main.go
+++ exp_grpc/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net"
+	"research/exp_grpc/proto"
+
+	"google.golang.org/grpc"
+)
+
+var addr = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Listening on", addr)
+
+	s := grpc.NewServer()
+
+	proto.RegisterGreeterServer(s, &Server{})
+
+	if err = s.Serve(lis); err != nil {
+		panic(err)
+	}
+}
