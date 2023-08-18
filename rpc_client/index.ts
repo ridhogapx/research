be61df134dc9108d21c08f37d7c7e6da7a80bf5e
RageNeko26 <rneko2006@gmail.com>
2023-08-18 13:54:48 +0700

    feat: add rpc client

---
+++ rpc_client/index.ts
@@ -0,0 +1,22 @@
+const path = require('path');
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+
+const PROTO_PATH = path.resolve(__dirname, "auth.proto");
+
+const options = {
+    keepCase: true,
+    longs: String, 
+    enums: String, 
+    defaults: true,
+    oneofs: true,
+};
+
+const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const proto = grpc.loadPackageDefinition(packageDefinition).proto;
+
+const client = new proto.AuthService(
+    "localhost:50052",
+    grpc.credentials.createInsecure()
+)
+
