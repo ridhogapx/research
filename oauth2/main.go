c1a3b522a0096224aca83ad9d3499954ad23d155
RageNeko26 <rneko2006@gmail.com>
2023-08-26 16:22:57 +0700

    feat: Finishing sample

--- oauth2/main.go
+++ oauth2/main.go
@@ -2,32 +2,34 @@ package main
 
 import (
 	"context"
-	"errors"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 )
 
-var oauthConfig *oauth2.Config
-
-func init() {
-	oauthConfig = &oauth2.Config{
-		RedirectURL:  "localhost:8080/callback",
-		ClientID:     "60848798350-v9dqmsun419pgk5ie3tj6hlk7hn8uqbu.apps.googleusercontent.com",
-		ClientSecret: "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",
-		Scopes:       []string{"https://www.googleapis.com/auth/blogger"},
-		Endpoint:     google.Endpoint,
-	}
+var oauthConfig = &oauth2.Config{
+	RedirectURL:  "http://localhost:8080/auth/google/callback",
+	ClientID:     "60848798350-v9dqmsun419pgk5ie3tj6hlk7hn8uqbu.apps.googleusercontent.com",
+	ClientSecret: "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",
+	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	Endpoint:     google.Endpoint,
 }
 
-var randStateString = "asdasdlkajsdfkasjdfvn,mn@#@$"
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
 
 func main() {
-	http.HandleFunc("/login", handleGoogleLogin)
-	http.HandleFunc("/callback", handleCallbackGoogle)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome OAUTH2")
+	})
+	http.HandleFunc("/auth/google/login", handleGoogleLogin)
+	http.HandleFunc("/auth/google/callback", handleCallbackGoogle)
 
 	fmt.Println("Server running on port 8080")
 	http.ListenAndServe(":8080", nil)
@@ -35,50 +37,58 @@ func main() {
 
 func handleGoogleLogin(w http.ResponseWriter, r *http.Request) {
 	url := oauthConfig.AuthCodeURL("state")
-	fmt.Println("Visit url:", url)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
 func handleCallbackGoogle(w http.ResponseWriter, r *http.Request) {
-	content, err := getUserInfo(r.FormValue("state"), r.FormValue("code"))
+	// Read oauthState from Cookie
 
-	if err != nil {
-		fmt.Println(err)
+	if r.FormValue("state") != "state" {
+		log.Println("invalid oauth google state")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
 		return
 	}
 
-	fmt.Printf("Content: %v", content)
+	data, err := getUserDataFromGoogle(r.FormValue("code"))
+	if err != nil {
+		log.Println(err.Error())
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
 
+	// GetOrCreate User in your db.
+	// Redirect or response with a token.
+	// More code .....
+	fmt.Fprintf(w, "UserInfo: %s\n", data)
 }
 
-func getUserInfo(state string, code string) ([]byte, error) {
-	if state != randStateString {
-		return nil, errors.New("invalid oauth state" + state)
-	}
+func getUserDataFromGoogle(code string) ([]byte, error) {
+	// Use code to get token and get user info from Google.
 
 	token, err := oauthConfig.Exchange(context.Background(), code)
-
 	if err != nil {
-		return nil, errors.New("code exchange failed")
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
 	}
-
-	oauthGoogleUrlAPI := "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
-
-	client := oauthConfig.Client(context.Background(), token)
-
-	response, err := client.Get(oauthGoogleUrlAPI + token.AccessToken)
-
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
 	if err != nil {
-		return nil, errors.New("failed getting user info")
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
 	}
-
 	defer response.Body.Close()
 	contents, err := ioutil.ReadAll(response.Body)
-
 	if err != nil {
-		return nil, errors.New("failed reading response body")
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
 	}
-
 	return contents, nil
+}
 
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(365 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
 }
