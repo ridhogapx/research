b2421d8f0fcef2944ffd79159c3dd509e3b27db8
RageNeko26 <rneko2006@gmail.com>
2023-09-07 18:32:58 +0700

    test: should be insert data

--- cv/db/sqlc/main_test.go
+++ cv/db/sqlc/main_test.go
@@ -13,7 +13,7 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	testDB, err = sql.Open("postgres", "postgresql://root:root@localhost:5432/cv")
+	testDB, err = sql.Open("postgres", "postgresql://root:root@localhost:5432/cv?sslmode=disable")
 
 	if err != nil {
 		fmt.Println("Failed to connect Database")
--- cv/db/sqlc/user_test.go
+++ cv/db/sqlc/user_test.go
@@ -2,8 +2,12 @@ package db
 
 import (
 	"testing"
+	"context"
 )
 
 func TestCreateUser(t *testing.T) {
-	
+	err := testQueries.CreateUser(context.Background(), "foo@mai.com")
+	if err != nil {
+		t.Error(err)
+	}
 }
